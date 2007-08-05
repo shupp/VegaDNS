f9e29c77d95366e0c76f4f87bb954df25a72adc7
Bill Shupp <hostmaster@shupp.org>
2007-08-05 00:50:05 +0000

    first draft of Domains module, still broken

---
+++ Framework/Module/Domains.php
@@ -0,0 +1,135 @@
+<?php
+
+
+class Framework_Module_Domains extends VegaDNS_Common
+{
+
+    public function __constructor()
+    {
+        $group = $this->session->groupID;
+        $group_name_array = $this->user->returnGroup($group,NULL);
+        $this->data['group_name'] = $group_name_array['name'];
+        $this->data['group'] = $group;
+
+        if ($this->user->getBit('domain_create')) {
+            $smarty->assign('new_domain_url', $base_url."&module=Domains&domain_mode=add");
+        }
+
+        if ($this->user->getBit('domain_edit')) {
+            $smarty->assign('edit_domain_url_base', $base_url."&module=Domains&domain_mode=edit_domain");
+        }
+    }
+
+    public function __default()
+    {
+        // Get search string if it exists
+        if (isset($_REQUEST['search']) && $_REQUEST['search'] != "") {
+            $tempstring = ereg_replace("[*]", "%", $_REQUEST['search']);
+            $tempstring = ereg_replace("[ ]", "%", $tempstring);
+            $searchstring = "and domain like ".$db->Quote('%'.$tempstring.'%')."";
+    
+            $smarty->assign('search', $_REQUEST['search']);
+            $smarty->assign('searchtexttag', " matching \"".$_REQUEST['search']."\""
+    );
+            $search = $_REQUEST['search'];
+        } else {
+            $searchstring = "";
+            $search = "";
+        }
+    
+        // Get scope of domain list, if it exists
+        if (isset($_REQUEST['recursive'])) {
+            $groupquery = $my->returnSubgroupsQuery($my->returnGroup($group, NULL), NULL);
+            $smarty->assign('recursive', ' checked');
+        } else {
+            $groupquery = " a.group_id='$group'";
+        }
+    
+        // Get scope of domain list, if it exists
+        if (isset($_REQUEST['scope']) && $_REQUEST['scope'] != "") {
+            $searchstring = "";
+            $search = "";
+            $scope = $_REQUEST['scope'];
+            $smarty->assign('scope', $_REQUEST['scope']);
+    
+            if ($scope != "num") {
+                    $sq = " and domain regexp \"^[$scope" . strtoupper($scope) . "]\"";
+            } else {
+                    $sq = "and domain regexp \"^[0-9]\"";
+            }
+        } else {
+            $sq = "";
+        }
+    
+        // Show domain list
+    
+        $q = "SELECT a.*, b.group_id, b.name 
+                FROM domains a 
+                LEFT JOIN groups b ON a.group_id = b.group_id 
+                WHERE ($groupquery) $searchstring $sq ";
+    
+        // sort
+        $sortway = $this->getRequestSortWay();
+        $sortfield = $this->getSortfield('domains');
+    
+        $q .= "order by $sortfield $sortway ".( ($sortfield == "status") ? ", domain" : "" )."";
+    
+        $result = $this->db->Execute($q) or die($db->ErrorMsg()."q: $q");
+        $totalitems = $result->RecordCount();
+    
+        // sort
+        $sort_array['Domain'] = 'domain';
+        $sort_array['Status'] = 'status';
+        $sort_array['Group'] = 'group_id';
+    
+        $sortbaseurl = "$base_url&module=Domains&page=".( ((isset($_REQUEST['page']) && $_REQUEST['page'] == 'all')) ? "all" : $page);
+    
+        while (list($key,$val) = each($sort_array)) {
+            $newsortway = $this->getSortWay($sortfield, $val, $sortway);
+            $url = "<a href='$sortbaseurl&sortway=$newsortway&sortfield=$val'>".ereg_replace('_', ' ', $key)."</a>";
+            if ($sortfield == $val) $url .= "&nbsp;<img border=0 alt='$sortway' src=images/$sortway.png>";
+            $this->data[$key] = $url;
+        }
+    
+        if ($totalitems > 0) {
+    
+    
+            $domain_count = 0;
+            // Actually list domains
+            while (++$domain_count && !$result->EOF && ($row = $result->FetchRow())
+                && ($domain_count <= $last_item)) {
+    
+                if ($domain_count < $first_item) continue;
+    
+                $out_array[$domain_count]['domain'] = $row['domain'];
+                $out_array[$domain_count]['edit_url'] = "$base_url&mode=records&domain=".$row['domain'];
+                $out_array[$domain_count]['status'] = $row['status'];
+                $out_array[$domain_count]['group_name'] = get_groupowner_name($row['group_id']);
+                if ($user_info['account_type'] == 'senior_admin' || $user_info['account_type'] == 'group_admin') {
+                    $out_array[$domain_count]['change_owner_url'] = "$base_url&module=Domains&domain_mode=change_owner&domain_id=".$row['domain_id']."&domain=".$row['domain'];
+                }
+                if ($row['status'] == 'inactive') {
+                    if ($user_info['account_type'] == 'senior_admin') {
+                        $out_array[$domain_count]['activate_url'] = "$base_url&module=Domains&domain_mode=activate_domain&domain_id=".$row['domain_id']."&domain=".$row['domain'];
+                    }
+                } else if ($row['status'] == 'active') {
+                    if ($user_info['account_type'] == 'senior_admin') {
+                        $out_array[$domain_count]['deactivate_url'] = "$base_url&module=Domains&domain_mode=deactivate_domain&domain_id=".$row['domain_id']."&domain=".$row['domain'];
+                    }
+                }
+                $out_array[$domain_count]['delete_url'] = "$base_url&module=Domains&domain_mode=delete&domain_id=".$row['domain_id']."&domain=".$row['domain'];
+            }
+        }
+    
+        $this->data['all_url'] = "$base_url&module=Domains&page=all&sortfield=$sortfield&sortway=$sortway&search=".urlencode($search);
+        $this->data['first_item'] = $first_item;
+        $this->data['last_item'] = $last_item;
+        $this->data['totalitems'] = $totalitems;
+        $this->data['totalpages'] = $totalpages;
+        if (isset($out_array)) {
+            $smarty->assign('out_array', $out_array);
+        }
+    
+    }
+}
+?>
---
+++ Framework/Module/Domains/Templates/Default/Domains.tpl
@@ -0,0 +1,91 @@
+    <table width="90%">
+    <tr>
+    <td class="border">
+    <table width="100%" border="0" cellspacing="5" cellpadding="3" bgcolor="white">
+        <tr>
+            <td colspan="6" align="center">
+
+            Domains in group {$group_name}<p>
+            <table width="100%" border=0 cellspacing=0 cellpadding=3>
+                <tr valign="top">
+                <td align="left" colspan="2">
+
+                Listing {$first_item} - {$last_item} of {$totalitems} Domains {$searchtexttag}</td>
+                <td align="center" colspan="2">
+                {if $previous_url} <a href={$previous_url}>previous</a>
+                {else}previous{/if}
+                {if $next_url} <a href={$next_url}>next</a>
+                {else}next{/if}
+                {if $first_url} <a href={$first_url}>first</a>
+                {else}first{/if}
+                {if $last_url} <a href={$last_url}>last</a>
+                {else}last{/if}
+                <a href={$all_url}>all</a>
+                </td>
+                <td align="right" colspan="2">
+                    <form action="{$php_self}">
+                    <input type="hidden" name="state" value="{$state}">
+                    <input type="hidden" name="mode" value="domains">
+                    <input type="hidden" name="{$session_name}" value="{$session_id}">
+                    <input type="hidden" name="group" value="{$group}">
+                    search sub domains <input type="checkbox" name="recursive" {$recursive}>
+                    <input type="text" name="search" value="{$search}">
+                    <input type="submit" value="search"></form>
+
+                </td>
+                </tr>
+               <tr>
+                <td nowrap align="center" colspan="6" width=100%>
+<a href="{$all_url}">ALL</a> | <a href="{$all_url}&scope=num">0-9</a> | <a href="{$all_url}&scope=a">A</a> | <a href="{$all_url}&scope=b">B</a> | <a href="{$all_url}&scope=c">C</a> | <a href="{$all_url}&scope=d">D</a> | <a href="{$all_url}&scope=e">E</a> | <a href="{$all_url}&scope=f">F</a> | <a href="{$all_url}&scope=g">G</a> | <a href="{$all_url}&scope=h">H</a> | <a href="{$all_url}&scope=i">I</a> | <a href="{$all_url}&scope=j">J</a> | <a href="{$all_url}&scope=k">K</a> | <a href="{$all_url}&scope=l">L</a> | <a href="{$all_url}&scope=m">M</a> | <a href="{$all_url}&scope=n">N</a> | <a href="{$all_url}?&scope=o">O</a> | <a href="{$all_url}&scope=p">P</a> | <a href="{$all_url}&scope=q">Q</a> | <a href="{$all_url}&scope=r">R</a> | <a href="{$all_url}&scope=s">S</a> | <a href="{$all_url}&scope=t">T</a> | <a href="{$all_url}&scope=u">U</a> | <a href="{$all_url}&scope=v">V</a> | <a href="{$all_url}&scope=w">W</a> | <a href="{$all_url}&scope=x">X</a> | <a href="{$all_url}&scope=y">Y</a> | <a href="{$all_url}&scope=z">Z</a>
+                </td>
+               </tr>
+
+            </table>
+
+
+            </td>
+        </tr>
+        <tr><td colspan="4" align="right">{if $new_domain_url}<a href="{$new_domain_url}">New Domain</a>{else}New Domain{/if}</td>
+        </tr>
+        <tr>
+            <td class="underline" nowrap>{$Domain}</td><td class="underline" nowrap>{$Status}</td>
+            <td class="underline" align="center" nowrap>{$Group}</td>
+            <td class="underline" align="center" nowrap>Change Status</td>
+            <td class="underline" width="1%">Delete</td>
+        </tr>
+
+        {foreach from=$out_array item=row}
+        <tr bgcolor="{cycle values="#dcdcdc,#ffffff"}">
+            <td><a href="{$row.edit_url}">{$row.domain}</a></td>
+            <td width="1%" nowrap>{$row.status}</td>
+            <td width="1%" align="center" nowrap>
+            {if $row.change_owner_url}
+                <a href="{$row.change_owner_url}">{$row.group_owner_name}</a>
+            {else}
+                {$row.group_owner_name}
+            {/if}
+            </td>
+            <td width="1%" align="center">
+            {strip}
+            {if $row.status == "active"}
+                {if $row.deactivate_url}
+                    <a href="{$row.deactivate_url}">de-activate</a>
+                {else}
+                    de-activate
+                {/if}
+            {else if $row.state == "inactive"}
+                {if $row.activate_url}
+                    <a href="{$row.activate_url}">activate</a>
+                {else}
+                    activate
+                {/if}
+            {/if}
+            {/strip}
+            </td>
+            <td align="center" width="1%"><a href="{$row.delete_url}"><img src="images/trash.png" border=0 alt="Trash"></a></td>
+            </tr>
+        {/foreach}
+    </table>
+    </td>
+    </tr>
+    </table>
