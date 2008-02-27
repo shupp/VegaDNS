b994d5b8af9da4aa0529563c093eab6a4f619823
Bill Shupp <hostmaster@shupp.org>
2008-02-27 06:43:04 +0000

    cleanups in Framework_Module_Login

--- Framework/Module/Login.php
+++ Framework/Module/Login.php
@@ -4,23 +4,42 @@
 /**
  * Framework_Module_Login
  *
- * @author      Bill Shupp <hostmaster@shupp.org>
- * @copyright   Bill Shupp <hostmaster@shupp.org>
- * @package     VegaDNS
- * @subpackage  Module
+ * Provide login/logout functionality
+ *
+ * PHP Version 5
+ *
+ * @category   Net
+ * @package    VegaDNS
+ * @subpackage Module
+ * @author     Bill Shupp <hostmaster@shupp.org>
+ * @copyright  2008 Bill Shupp <hostmaster@shupp.org>
+ * @license    GPL http://www.gnu.org/copyleft/gpl.html
+ * @link       http://vegadns.org/
  * @filesource
  */
 
 /**
  * Framework_Module_Login
  *
- * @author      Bill Shupp <hostmaster@shupp.org>
- * @package     VegaDNS
- * @subpackage  Module
+ * @category   Net
+ * @package    VegaDNS
+ * @subpackage Module
+ * @author     Bill Shupp <hostmaster@shupp.org>
+ * @copyright  2008 Bill Shupp <hostmaster@shupp.org>
+ * @license    GPL http://www.gnu.org/copyleft/gpl.html
+ * @link       http://vegadns.org/
  */
 class Framework_Module_Login extends Framework_Auth_No
 {
 
+    /**
+     * __construct 
+     * 
+     * Set pageTemplateFile
+     * 
+     * @access public
+     * @return void
+     */
     public function __construct()
     {
         parent::__construct();
@@ -30,22 +49,32 @@ class Framework_Module_Login extends Framework_Auth_No
     /**
      * __default
      *
+     * Display Login.tpl
+     *
      * @access      public
      * @return      mixed
      */
     public function __default()
     {
-        $form = $this->createLoginForm();
+        $form = $this->_createLoginForm();
         $this->setData('QF_Form', $form->toHtml());
         $this->tplFile = 'Login.tpl';
     }
 
+    /**
+     * loginNow 
+     * 
+     * Try and log the user in.
+     * 
+     * @access public
+     * @return void
+     */
     public function loginNow()
     {
-        $form = $this->createLoginForm();
+        $form = $this->_createLoginForm();
         if ($form->validate()) {
             $result = $this->user->authenticate($_POST['email'], $_POST['password']);
-            if ($result !== TRUE) {
+            if ($result !== true) {
                 $this->setData('loginError', _("Login failed"));
                 $this->setData('QF_Form', $form->toHtml());
                 $this->session->__set('userID', null);
@@ -59,32 +88,63 @@ class Framework_Module_Login extends Framework_Auth_No
         }
     }
 
-    private function createLoginForm()
+    /**
+     * _createLoginForm 
+     * 
+     * Create HTML_QuickForm object for logging in
+     * 
+     * @access private
+     * @return HTML_Quickform object
+     */
+    private function _createLoginForm()
     {
-        $form = new HTML_QuickForm('formLogin', 'post', $_SERVER['REQUEST_URI'] . '&event=loginNow');
+        $form = new HTML_QuickForm('formLogin',
+            'post', $_SERVER['REQUEST_URI'] . '&event=loginNow');
 
         $form->addElement('header', 'MyHeader', _('Login'));
         $form->addElement('text', 'email', _('Email'));
         $form->addElement('password', 'password', _('Password'));
         $form->addElement('submit', 'submit', _('Login'));
 
-        $form->addRule('email', _('Please enter your email address'), 'required', null, 'client');
-        $form->addRule('email', _('Please enter a valid email address'), 'email', null, 'client');
-        $form->addRule('password', _('Please enter your password'), 'required', null, 'client');
+        $form->addRule('email',
+            _('Please enter your email address'), 'required', null, 'client');
+        $form->addRule('email',
+            _('Please enter a valid email address'), 'email', null, 'client');
+        $form->addRule('password',
+            _('Please enter your password'), 'required', null, 'client');
         $form->applyFilter('__ALL__', 'trim');
 
         return $form;
     }
 
-    function logoutNow() {
+    /**
+     * logoutNow 
+     * 
+     * Log user out and run $this->__default()
+     * 
+     * @access public
+     * @return void
+     */
+    public function logoutNow()
+    {
         $this->session->destroy();
         $this->setData('message', _('Logged out successfully'));
         return $this->__default();
     }
 
-    function logoutInactive() {
+    /**
+     * logoutInactive 
+     * 
+     * Log out user for inactivity
+     * 
+     * @access public
+     * @return $this->__default()
+     */
+    public function logoutInactive()
+    {
         $this->session->destroy();
-        $this->setData('message', _('You have been logged out automatically for inactivity'));
+        $this->setData('message',
+            _('You have been logged out automatically for inactivity'));
         return $this->__default();
     }
 
