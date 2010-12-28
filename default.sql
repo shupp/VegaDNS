24976dd3b6936136befb054406c1837778692206
Bill Shupp <hostmaster@shupp.org>
2010-12-27 22:15:19 -0800

    Created default.sql from create_tables.php

--- default.sql
+++ default.sql
@@ -1,32 +1,5 @@
-<?php
-
-
-/*
- * 
- * VegaDNS - DNS Administration Tool for use with djbdns
- * 
- * CREDITS:
- * Written by Bill Shupp
- * <hostmaster@shupp.org>
- * 
- * LICENSE:
- * This software is distributed under the GNU General Public License
- * Copyright 2003-2010, Bill Shupp
- * see COPYING for details
- * 
- */ 
-
-if(!ereg(".*/index.php$", $_SERVER['PHP_SELF'])) {
-    header("Location:../index.php");
-    exit;
-}
-
-
-
-
-
-
-$q = "CREATE TABLE accounts (
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE accounts (
   cid int(10) NOT NULL auto_increment,
   gid int(10) default '0',
   Email varchar(60) NOT NULL default '',
@@ -37,44 +10,40 @@ $q = "CREATE TABLE accounts (
   Account_Type enum('user','group_admin','senior_admin') NOT NULL default 'user',
   Status enum('active','inactive') NOT NULL default 'inactive',
   UNIQUE KEY cid (cid,Email)
-) TYPE=MyISAM";
-mysql_query($q) or die(mysql_error());
+) TYPE=MyISAM;
 
-$q = "INSERT INTO accounts VALUES (0,0,'test@test.com','".md5('test')."','Test','User','','senior_admin','active')";
-mysql_query($q) or die(mysql_error()."<br>".$q);
+INSERT INTO accounts VALUES (0,0,'test@test.com','098f6bcd4621d373cade4e832627b4f6','Test','User','','senior_admin','active');
 
-$q = "CREATE TABLE active_sessions (
+DROP TABLE IF EXISTS `active_sessions`;
+CREATE TABLE active_sessions (
   sid varchar(32) NOT NULL default '',
   Email varchar(60) NOT NULL default '',
   time int(14) NOT NULL default '0',
   PRIMARY KEY  (sid)
-) TYPE=MyISAM";
-mysql_query($q) or die(mysql_error());
+) TYPE=MyISAM;
 
-$q = "INSERT INTO active_sessions VALUES ('".session_id()."','test@test.com',".time().")";
-mysql_query($q) or die(mysql_error());
-
-$q = "CREATE TABLE domains (
+DROP TABLE IF EXISTS `domains`;
+CREATE TABLE domains (
   domain_id int(11) NOT NULL auto_increment,
   domain varchar(100) NOT NULL default '',
   owner_id int(11) default NULL,
   group_owner_id int(11) default NULL,
   status enum('active','inactive') NOT NULL default 'inactive',
   KEY domain_id (domain_id,domain)
-) TYPE=MyISAM";
-mysql_query($q) or die(mysql_error());
+) TYPE=MyISAM;
 
-$q = "CREATE TABLE log (
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE `log` (
   domain_id int(11) NOT NULL default '0',
   cid int(11) NOT NULL default '0',
   Email varchar(60) NOT NULL default '',
   Name varchar(60) NOT NULL default '',
   entry varchar(200) NOT NULL default '',
   time int(11) NOT NULL default '0'
-) TYPE=MyISAM";
-mysql_query($q) or die(mysql_error());
+) TYPE=MyISAM;
 
-$q = "CREATE TABLE records (
+DROP TABLE IF EXISTS `records`;
+CREATE TABLE `records` (
   domain_id int(11) NOT NULL default '0',
   record_id int(11) NOT NULL auto_increment,
   host varchar(100) NOT NULL default '',
@@ -86,10 +55,10 @@ $q = "CREATE TABLE records (
   ttl int(11) NOT NULL default '86400',
   UNIQUE KEY records_id (record_id),
   KEY records_idx (domain_id,record_id,host)
-) TYPE=MyISAM";
-mysql_query($q) or die(mysql_error());
+) TYPE=MyISAM;
 
-$q = "CREATE TABLE default_records (
+DROP TABLE IF EXISTS `default_records`;
+CREATE TABLE `default_records` (
   record_id int(11) NOT NULL auto_increment,
   group_owner_id int(11) default NULL,
   host varchar(100) NOT NULL default '',
@@ -101,9 +70,6 @@ $q = "CREATE TABLE default_records (
   ttl int(11) NOT NULL default '86400',
   default_type enum('system','group') NOT NULL default 'system',
   UNIQUE KEY records_id (record_id)
-) TYPE=MyISAM";
-mysql_query($q) or die(mysql_error());
-$q = "INSERT INTO default_records VALUES (1,0,'hostmaster.DOMAIN:ns1.myserver.com','S','16384:2048:1048576:2560',0,'','',86400,'system')";
-mysql_query($q) or die(mysql_error()."<br>".$q);
+) TYPE=MyISAM;
 
-?>
+INSERT INTO default_records VALUES (1,0,'hostmaster.DOMAIN:ns1.myserver.com','S','16384:2048:1048576:2560',0,'','',86400,'system');
