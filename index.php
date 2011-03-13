448ce600a270492e18587fdae9a3c880352a299c
Bill Shupp <hostmaster@shupp.org>
2011-03-12 21:55:40 -0800

    Set session.use_only_cookies to 0, as it defaults to 1 since 5.3.3, breaking things

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ ini_set('display_startup_errors', 0);
 ini_set('log_errors', 1);
 ini_set('allow_url_fopen', 0);
 ini_set('session.use_cookies',0);
+ini_set('session.use_only_cookies', 0);
 ini_set('error_reporting', E_ALL & ~E_DEPRECATED);
 
 // Check that register_globals is off
