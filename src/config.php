38ff1dda78bcf26f42d6110fc5c2ed7bdc0b0d20
Bill Shupp <hostmaster@shupp.org>
2012-02-25 11:25:25 -0800

    Updated CHANGELOG for 0.11.1 release Cleaned up whitespace in CHANGELOG, README, INSTALL, UPGRADE Updated src/config.php version

--- src/config.php
+++ src/config.php
@@ -37,7 +37,7 @@ $mysql_pass = 'secret';
 $mysql_db = 'vegadns';
 
 // Local URL
-$vegadns_url = 'http://127.0.0.1/vegadns-0.11.0/';
+$vegadns_url = 'http://127.0.0.1/vegadns-0.11.1/';
 
 // Contact info used in from/to addresses of email notifactions for inactive
 // domains
@@ -71,7 +71,7 @@ $dns_tools_dir = '/usr/local/bin';
 // NO NEED TO EDIT BELOW THIS LINE //
 /////////////////////////////////////
 
-$version = '0.11.0';
+$version = '0.11.1';
 
 if(!ereg(".*/index.php$", $_SERVER['PHP_SELF'])
     && !ereg(".*/axfr_get.php$", $_SERVER['PHP_SELF'])) {
