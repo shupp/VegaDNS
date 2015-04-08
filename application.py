e2e490482b739ee4e47e60a69ccaf3c0c8ab20a1
Bill Shupp <hostmaster@shupp.org>
2015-04-08 12:45:58 -0700

    Added test dependencies

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask, abort, redirect, url_for, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('domains'))
+
+@app.route('/v1/domains')
+def domains():
+    domain_list = [{'id': 1, 'name': 'vegadns.org', 'owner_id': 0}]
+    return jsonify(status='ok', domains=domain_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
