f3262473c20228bab347cf36da655040c4981f50
Bill Shupp <hostmaster@shupp.org>
2015-04-08 13:42:26 -0700

    Added a simple unit test for /domains

---
+++ test_application.py
@@ -0,0 +1,12 @@
+import unittest, json
+from application import app
+
+class TestDomains(unittest.TestCase):
+    def test_get(self):
+        # Use Flask's test client
+        self.test_app = app.test_client()
+        response = self.test_app.get('/v1/domains')
+        self.assertEqual(response.status, "200 OK")
+        decoded = json.loads(response.data)
+        self.assertEqual(decoded['status'], "ok")
+        self.assertEqual(decoded['domains'][0]['id'], 1)
