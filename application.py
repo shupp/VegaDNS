72910683df98f6e60a602c5a7256e1d05ae6520b
Bill Shupp <hostmaster@shupp.org>
2015-04-08 13:42:46 -0700

    Be explicit about supported HTTP methods

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from flask import Flask, abort, redirect, url_for, jsonify
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return redirect(url_for('domains'))
 
-@app.route('/v1/domains')
+@app.route('/v1/domains', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def domains():
     domain_list = [{'id': 1, 'name': 'vegadns.org', 'owner_id': 0}]
     return jsonify(status='ok', domains=domain_list)
