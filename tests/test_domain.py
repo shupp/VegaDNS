0446aeb345dae294b3ea30bdea9c53b12d8f4cf9
Bill Shupp <hostmaster@shupp.org>
2015-04-15 14:41:59 -0700

    Updated tests to work with mocked data

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,15 +1,31 @@
 import unittest
 import json
+from mock import MagicMock
+
 from vegadns.api import app
-from vegadns.api.endpoints import domain
+import vegadns.api.endpoints.domain
 
 
 class TestDomain(unittest.TestCase):
-    def test_get(self):
+    def test_get_success(self):
+        # mock get_domain and to_dict
+        mock_value = {
+            'owner': 0,
+            'status': 'active',
+            'group_owner': 0,
+            'domain': 'vegadns.org',
+            'domain_id': 1
+        }
+        mock_model = MagicMock()
+        mock_model.to_dict = MagicMock(return_value = mock_value)
+        vegadns.api.endpoints.domain.Domain.get_domain = MagicMock(
+            return_value = mock_model
+        )
+
         # Use Flask's test client
         self.test_app = app.test_client()
-        response = self.test_app.get('/domains/1234')
+        response = self.test_app.get('/domains/1')
         self.assertEqual(response.status, "200 OK")
         decoded = json.loads(response.data)
         self.assertEqual(decoded['status'], "ok")
-        self.assertEqual(decoded['domain']['id'], 1234)
+        self.assertEqual(decoded['domain']['domain_id'], 1)
--- tests/test_domains.py
+++ tests/test_domains.py
@@ -1,17 +1,46 @@
 import unittest
+from mock import MagicMock
 import json
 from vegadns.api import app
-from vegadns.api.endpoints import domains
+import vegadns.api.endpoints.domains
 
 
 class TestDomains(unittest.TestCase):
-    def test_get(self):
+    def test_get_success(self):
+        mock_domain_one = {
+            'owner': 0,
+            'status': 'active',
+            'group_owner': 0,
+            'domain': 'vegadns.org',
+            'domain_id': 1
+        }
+        mock_model_one = MagicMock()
+        mock_model_one.to_dict = MagicMock(return_value = mock_domain_one)
+
+        mock_domain_two = {
+            'owner': 0,
+            'status': 'active',
+            'group_owner': 0,
+            'domain': 'vegadns.net',
+            'domain_id': 2
+        }
+        mock_model_two = MagicMock()
+        mock_model_two.to_dict = MagicMock(return_value = mock_domain_two)
+
+        vegadns.api.endpoints.domains.Domains.get_domain_list = MagicMock(
+            return_value = [mock_model_one, mock_model_two]
+        )
+
         # Use Flask's test client
         self.test_app = app.test_client()
         response = self.test_app.get('/domains')
         self.assertEqual(response.status, "200 OK")
+
         decoded = json.loads(response.data)
         self.assertEqual(decoded['status'], "ok")
-        self.assertEqual(decoded['domains'][0]['id'], 1)
-        self.assertEqual(decoded['domains'][0]['name'], 'vegadns.org')
-        self.assertEqual(decoded['domains'][0]['owner_id'], 0)
+        self.assertEqual(decoded['domains'][0]['domain_id'], 1)
+        self.assertEqual(decoded['domains'][0]['domain'], 'vegadns.org')
+        self.assertEqual(decoded['domains'][0]['owner'], 0)
+        self.assertEqual(decoded['domains'][1]['domain_id'], 2)
+        self.assertEqual(decoded['domains'][1]['domain'], 'vegadns.net')
+        self.assertEqual(decoded['domains'][1]['owner'], 0)
