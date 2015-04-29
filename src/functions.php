shupp/VegaDNS

commit 8143aee4e843145c93d24d120fa0b98191a83fb4

Added RFC 4408 support (SPF via type 99)

src/functions.php
@@ -193,6 +193,7 @@ function get_type($type) {
     if($type == 'T') return 'TXT';
     if($type == 'C') return 'CNAME';
     if($type == 'V') return 'SRV';
+    if($type == 'F') return 'SPF';
 
 }
 
@@ -208,6 +209,7 @@ function set_type($type) {
     if($type == 'TXT') return 'T';
     if($type == 'CNAME') return 'C';
     if($type == 'SRV') return 'V';
+    if($type == 'SPF') return 'F';
 
 }
 
@@ -575,6 +577,9 @@ function build_data_line($row,$domain) {
         $s = "^".$row['host'].":".$row['val'].":".$row['ttl']."\n";
     } else if($row['type'] == 'T') {
         $s = "'".$row['host'].":".str_replace(":",'\072', $row['val']).":".$row['ttl']."\n";
+    } else if($row['type'] == 'F') {
+        $val_str = str_replace(":",'\072', $row['val']);
+        $s = ":".$row['host'].":99:\\".str_pad(decoct(strlen($row['val'])),3,0,STR_PAD_LEFT)."".$val_str.":".$row['ttl']."\n";
     } else if($row['type'] == 'C') {
         $s = "C".$row['host'].":".$row['val'].":".$row['ttl']."\n";
     } else if($row['type'] == 'S') {
@@ -646,6 +651,14 @@ function parse_dataline($line) {
             // Is a TXT record
             $out_array['host'] = $array[0];
             $out_array['type'] = 'T';
+            $out_array['val'] = $array[2];
+            $out_array['distance'] = '';
+            $out_array['ttl'] = $array[3];
+        }
+        if($array[1] == '99') {
+            // Is a SPF record
+            $out_array['host'] = $array[0];
+            $out_array['type'] = 'F';
             $out_array['val'] = $array[2];
             $out_array['distance'] = '';
             $out_array['ttl'] = $array[3];

src/add_record_form.php
@@ -32,15 +32,15 @@ if(isset($_REQUEST['name'])) {
 
 if($_REQUEST['mode'] == 'records') {
     if ($use_ipv6) {
-        $smarty->assign('typearray', array('A','AAAA','AAAA+PTR','NS','MX','PTR','TXT','CNAME','SRV'));
+        $smarty->assign('typearray', array('A','AAAA','AAAA+PTR','NS','MX','PTR','TXT','CNAME','SRV','SPF'));
     } else {
-        $smarty->assign('typearray', array('A','NS','MX','PTR','TXT','CNAME','SRV'));
+        $smarty->assign('typearray', array('A','NS','MX','PTR','TXT','CNAME','SRV','SPF'));
     }
 } else if ($_REQUEST['mode'] == 'default_records') {
     if ($use_ipv6) {
-        $smarty->assign('typearray', array('A','AAAA','AAAA+PTR','NS','MX','TXT','CNAME','SRV'));
+        $smarty->assign('typearray', array('A','AAAA','AAAA+PTR','NS','MX','TXT','CNAME','SRV','SPF'));
     } else {
-        $smarty->assign('typearray', array('A','NS','MX','TXT','CNAME','SRV'));
+        $smarty->assign('typearray', array('A','NS','MX','TXT','CNAME','SRV','SPF'));
     }
 }
 if(isset($_REQUEST['type']))

src/records.php
@@ -410,6 +410,15 @@ if(!isset($_REQUEST['record_mode']) || $_REQUEST['record_mode'] == 'delete_cance
             '".$_REQUEST['ttl']."')";
 
         }
+        if($_REQUEST['type'] == 'SPF') {
+            $q = "insert into records
+            (domain_id,host,type,val,ttl) values(
+            '".get_dom_id($domain)."',
+            '$name',
+            '".set_type($_REQUEST['type'])."',
+            '".mysql_escape_string($_REQUEST['address'])."',
+            '".$_REQUEST['ttl']."')";
+        }
         mysql_query($q) or die(mysql_error());
         set_msg("Record added successfully!");
         header("Location: $base_url&mode=records&domain=".urlencode($domain));

commit 5b3f09986eff115641c9cb6848564e2fefdb2a4a

Fixed SPF (RR type 99) export issue introduced in 8143aee

src/functions.php
@@ -627,8 +627,7 @@ function build_data_line($row,$domain) {
     } else if($row['type'] == 'T') {
         $s = "'".$row['host'].":".str_replace(":",'\072', $row['val']).":".$row['ttl']."\n";
     } else if($row['type'] == 'F') {
-        $val_str = str_replace(":",'\072', $row['val']);
-        $s = ":".$row['host'].":99:\\".str_pad(decoct(strlen($row['val'])),3,0,STR_PAD_LEFT)."".$val_str.":".$row['ttl']."\n";
+        $s = ":".$row['host'].":99:".str_replace(":",'\072', $row['val']).":".$row['ttl']."\n";
     } else if($row['type'] == 'C') {
         $s = "C".$row['host'].":".$row['val'].":".$row['ttl']."\n";
     } else if($row['type'] == 'S') {